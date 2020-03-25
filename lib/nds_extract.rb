d0204d84a823a25883016f2b7386a6d00d3868fd
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-25 07:06:25 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
+#pp directors_database
+
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -26,6 +28,14 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  director_list = []
+  director_index = 0 
+  while director_index < source.length do
+    director_name = source[director_index][:name]
+    director_list << director_name
+    director_index += 1 
+  end
+  director_list
 end
 
 def total_gross(source)
@@ -38,6 +48,15 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total = 0 
+  name_index = 0 
+  while name_index < source.length do
+    director_name = list_of_directors(source)[name_index]
+    director_gross = directors_totals(source)[director_name]
+    total += director_gross
+    name_index += 1
+  end
+  total
 end
 
 
